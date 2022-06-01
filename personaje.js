c91ed216934ac1686fcc2fa8a30195c6d64f68bc
V1ll4a <92698844+V1ll4a@users.noreply.github.com>
2022-05-31 21:53:10 -0500

    Add files via upload

---
+++ personaje.js
@@ -0,0 +1,73 @@
+let canvas =document.querySelector("#myCanvas");
+let ctx= canvas.getContext("2d")
+class Personaje{
+    constructor(x,y,angle,r){
+        this.x=x
+        this.y=y
+        this.angle=angle
+        this.r=r
+    }
+    dibujarse(){
+ctx.translate(this.x,this.y);
+ctx.rotate(this.angle*Math.PI)
+ctx.beginPath();
+ctx.arc(0,0,this.r,0,2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(0,0,this.r/2,0.25*Math.PI,0.75*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(-15,-11,this.r/6,0.25*Math.PI,2.25*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(15,-11,this.r/6,0.25*Math.PI,2.25*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(-15,-11,this.r/15,0.25*Math.PI,2.25*Math.PI);
+ctx.stroke();
+ctx.fill();
+ctx.beginPath();
+ctx.arc(15,-11,this.r/15,0.25*Math.PI,2.25*Math.PI);
+ctx.stroke();
+ctx.fill();
+ctx.strokeStyle="black"
+ctx.beginPath()
+ctx.moveTo(-6,0)
+ctx.lineTo(7,6)
+ctx.lineTo(-6,10)
+ctx.stroke()
+ctx.strokeStyle="black"
+ctx.beginPath()
+ctx.moveTo(-18,0)
+ctx.lineTo(-18,1)
+ctx.stroke()
+ctx.strokeStyle="black"
+ctx.beginPath()
+ctx.moveTo(18,0)
+ctx.lineTo(18,1)
+ctx.stroke()
+ctx.translate(-this.x,-this.y)
+    }
+}
+let Jeoirginha = new Personaje(200,100,0,50)
+Jeoirginha.dibujarse();
+let Pablito = new Personaje(200,300,0,50)
+Pablito.dibujarse();
+
+let b= document.querySelector("body")
+b.addEventListener("click", function(e){
+    console.log("Se hizo click")
+    console.log(e)
+})
+let xM= e.offsetx
+let yM= e.offsety
+if((xM-Jeoirginha.x)**2+(yM-Jeoirginha.y)**2<=Jeoirginha**2){
+ctx.fillStyle="white"
+ctx.strokeStyle="white"
+ctx.beginPath()
+ctx.rect(300,-300,1000,1000)
+ctx.stroke()
+ctx.fill()
+}
+let contenedor =  document.querySelector("#contenedor")
+contenedor.innerHTML = "Clica en los personajes para que desaparezcan :DDDD";
